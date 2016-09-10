dde64e0cd2a3c81b627d850184f2ac65a1ad4742
Melissa414 <Missy41491@gmail.com>
2016-09-10 13:45:06 -0400

    changes to stylesheet

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
 	$('#runSearch').on('click', function() {
-        var p = $("#searchTerm)".val(); 
-        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + p + "&api_key=eb240f09ad3340b68c5c63dcc8bf269f";
+        var p = $("#searchTerm").val(); 
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" + 'eb240f09ad3340b68c5c63dcc8bf269f' + "&api_key=eb240f09ad3340b68c5c63dcc8bf269f";
 
         $.ajax({ 
                 url: queryURL,
@@ -10,7 +10,7 @@ $(document).ready(function(){
             })
         //when we're finished 'getting' the response
             .done(function(response) {
-         
+                var results = response.data;
                 }
 
             });
